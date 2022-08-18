5944d5426ea950aff9399dba906cd9fdb1fae1fb
Aswanth Alakkadan <aswanth682@gamil.com>
2022-08-18 07:14:49 +0530

    final touches-2

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ document.querySelectorAll("menu-btn").forEach( n => n.addEventListener("click" ,
   menu.classList.remove("active")
 }))
 
-//var load = document.getElementById("loader")
-//window.addEventListener("load", function (){
-//load.style.display = "none";
+var load = document.getElementById("loader")
+window.addEventListener("load", function (){
+load.style.display = "none";
   
 
-///)
+})
